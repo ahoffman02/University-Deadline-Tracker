c5b68d86f65cf1dc051c5da0e17b576d18776a92
RebelDot\Mihai.Radu <mihai.radu@rebeldot.com>
2021-11-06 19:59:47 +0200

    database and model creation
    - entities
    - data context
    - database initial migration
    - CONTRIBUTING.md - added db setup info

---
+++ UDT.Model/Entities/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UDT.Model.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { set; get; }
+        public string Description { set; get; }
+        public string Subject { set; get; }
+        public string GroupName { set; get; }
+        public DateTime Deadline { set; get; }
+    }
+}
